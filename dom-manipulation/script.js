b83ac796b0b54641a36d6e18f73087203bb23310
Prichard-1 <genarodube8@gmail.com>
2025-07-06 06:13:29 +0200

    add script filr

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -204,13 +204,4 @@ function restoreCurrentIndex() {
 
 function fetchQuotesFromServer() {
   return fetch("https://jsonplaceholder.typicode.com/posts?_limit=5")
-    .then(response => response.json())
-    .then(data => {
-      // Convert posts into quote format
-      return data.map(post => ({
-        text: post.title,
-        category: "ServerSync"
-      }));
-    });
-}
-
+    .then(response => response.j
